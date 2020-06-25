c7578d9895ebbb1e5ecb9eb712f4a4272ac2df23
unknown <React Course 2@Tonpai-Lenovo.local>
2020-06-25 11:51:05 +0700

    Start project

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {
+  useState,
+} from 'react';
+import { 
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Alert,
+} from 'react-native';
+
 
 export default function App() {
+  const [value, setValue] = useState(0);
+
+  function incrementValue(){
+    setValue(value + 1);
+  }
+
+  function decrementValue(){
+    if(value > 0) {
+      setValue(value - 1);
+    }
+    else {
+      Alert.alert(
+        "ค่าเป็นลบ",
+        "ไม่สามารถนับค่าติดลบได้"
+      );
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.txtCounter}>{value}</Text>
+      <View style={styles.btnEditCounterContainer}>
+        <Button title="บวก" onPress={() => incrementValue() }/>
+      </View>
+      <View style={styles.btnEditCounterContainer}>
+        <Button title="ลบ" onPress={() => decrementValue() }/>
+      </View>
     </View>
   );
 }
@@ -13,7 +46,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
+    marginLeft: 10,
+    marginRight: 10,
   },
+  txtCounter: {
+    alignSelf: 'center',
+    fontSize: 150,
+    marginBottom: 10,
+  },
+  btnEditCounterContainer: {
+    marginBottom: 8,
+  }
 });
